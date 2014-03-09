2daa1fd1885c62c2b3f3b8c0c9080f00e92deda3
John Schimmel <john.schimmel@gmail.com>
2014-03-09 13:48:37 -0400

    adding theme to blog

--- blog/views.py
+++ blog/views.py
@@ -4,15 +4,19 @@ from blog.models import Blog, Category
 from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
-    return render_to_response('index.html', {
+    template_data = {
         'categories': Category.objects.all(),
-        'posts': Blog.objects.all()[:5]
-    })
+        'posts': Blog.objects.all()[:5],
+        'baseContainerClasses' : ['blog_page']
+    }
+    return render_to_response('index.html',template_data)
 
 def view_post(request, slug):   
-    return render_to_response('blog_entry.html', {
-        'post': get_object_or_404(Blog, slug=slug)
-    })
+    template_data = {
+        'post': get_object_or_404(Blog, slug=slug),
+        'baseContainerClasses' : ['blog_page','blog_item']
+    }
+    return render_to_response('blog_entry.html', template_data)
 
 def view_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
