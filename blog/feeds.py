3cc0ae7e4e632ef3bc51a924b6b7dcd226e805a2
John Schimmel <john.schimmel@gmail.com>
2014-03-21 07:11:01 -0400

    rss feed fix needed year/month attributes

--- blog/feeds.py
+++ blog/feeds.py
@@ -8,7 +8,7 @@ class LatestEntriesFeed(Feed):
     description = "Test description ..."
 
     def items(self):
-        return Blog.objects.order_by('-posted')[:10]
+        return Blog.objects.order_by('-display_date')[:10]
 
     def item_title(self, item):
         return item.title
@@ -18,4 +18,4 @@ class LatestEntriesFeed(Feed):
 
     # item_link is only needed if NewsItem has no get_absolute_url method.
     def item_link(self, item):
-        return reverse('view_blog_post', args=[item.slug])
+        return reverse('view_blog_post', args=[item.display_date.year, item.display_date.strftime("%m"),item.slug])
