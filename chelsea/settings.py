7c8bbd50898f5819e55d09f897e411bc06d0c5c9
John Schimmel <john.schimmel@gmail.com>
2014-03-23 00:00:32 -0400

    suit config

--- chelsea/settings.py
+++ chelsea/settings.py
@@ -58,16 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'staticfiles'
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-     os.path.join(PROJECT_PATH, 'static')
-)
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = "pillow"
@@ -266,3 +257,8 @@ else:
         }
     }
     logger.info("REDISCLOUD CACHE")
+
+# Django suit
+SUIT_CONFIG = {
+    'ADMIN_NAME': 'hichelsea admin'
+}
